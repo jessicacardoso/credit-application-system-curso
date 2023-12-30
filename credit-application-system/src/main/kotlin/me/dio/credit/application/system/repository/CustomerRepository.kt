9abf9460371c93f2147361561cebb88588bde240
jessicacardoso <jessicasousa.pc@gmail.com>
2023-12-29 21:56:32 -0300

    configurações adicionais

--- credit-application-system/src/main/kotlin/me/dio/credit/application/system/repository/CustomerRepository.kt
+++ credit-application-system/src/main/kotlin/me/dio/credit/application/system/repository/CustomerRepository.kt
@@ -1,6 +1,8 @@
+package me.dio.credit.application.system.repository
+
 import me.dio.credit.application.system.entity.Customer
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface CustomerRepository : JpaRepository<Customer, Long>
+interface CustomerRepository: JpaRepository<Customer, Long>
